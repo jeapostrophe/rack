775993197b901dbd30365594320cc44a7eea8950
Jay McCarthy <jay.mccarthy@gmail.com>
2014-02-22 20:33:17 -0700

    small-step

--- rack-et/kernel.rkt
+++ rack-et/kernel.rkt
@@ -111,6 +111,119 @@
 (struct rack:val:prim rack:val
         (prim))
 
+;; State
+
+(struct rack:cstate
+        (code env store kont))
+(struct rack:vstate
+        (vals kont store))
+
+(define mt-ms (hash))
+
+(struct rack:k (e k ms f))
+(struct rack:k:top ())
+
+(define (k-mark-set k mk mv)
+  (match-define (rack:k e nk ms f) k)
+  (define ms+
+    (hash-set ms mk mv))
+  (rack:k e nk ms+ f))
+
+(struct rack:kf ())
+(struct rack:kf:if0 rack:kf
+        (zero nonzero))
+(struct rack:kf:let-values rack:kf
+        (ids body))
+(struct rack:kf:wcm:key rack:kf
+        (val body))
+(struct rack:kf:wcm:val rack:kf
+        (key body))
+(struct rack:kf:app:fun rack:kf
+        (args))
+(struct rack:kf:app:args rack:kf
+        (fun rvals args))
+
+(define (rack:state:app fun rvals args e k s)
+  (match args
+    [(list)
+     (define vals (list->vector (reverse rvals)))
+     (match fun
+       [(rack:val:clo e (rack:expr:lambda params rest-param body))
+        (define e+
+          (hash-set
+           (for/fold ([e+ e])
+               ([p (in-vector params)]
+                [v (in-vector vals)])
+             (hash-set e+ p v))
+           rest-param
+           (list-tail vals (length params))))
+        (rack:cstate body e+ s k)]
+       [(rack:val:prim prim)
+        (rack:vstate (prim vals) k s)])]
+    [(cons arg args)
+     (rack:cstate arg e s
+                  (rack:k e k mt-ms
+                          (rack:kf:app:args fun rvals args)))]))
+
+(define (step st)
+  (match st
+    [(rack:cstate c e s k)
+     (match c
+       [(rack:expr:lambda params rest-param body)
+        (rack:vstate (vector (rack:val:clo e c)) k s)]
+       [(rack:expr:if0 test zero nonzero)
+        (rack:cstate test e s
+                     (rack:k e k mt-ms
+                             (rack:kf:if0 zero nonzero)))]
+       [(rack:expr:let-values ids vals body)
+        (rack:cstate vals e s
+                     (rack:k e k mt-ms
+                             (rack:kf:let-values ids body)))]
+       [(rack:expr:quote d)
+        (rack:vstate (vector d) k s)]
+       [(rack:expr:wcm mk mv body)
+        (rack:cstate mk e s
+                     (rack:k e k mt-ms
+                             (rack:kf:wcm:key mv body)))]
+       [(rack:expr:app fun args)
+        (rack:cstate fun e s
+                     (rack:k e k mt-ms
+                             (rack:kf:app:fun args)))]
+       [(rack:expr:ref v)
+        (rack:vstate (vector (hash-ref e v)) k s)])]
+    [(rack:vstate vs (rack:k:top) s)
+     st]
+    [(rack:vstate vs (rack:k e k ms kf) s)
+     (define (v) (vector-ref vs 0))
+     (match kf
+       [(rack:kf:if0 zero nonzero)
+        (define next
+          ;; xxx fix
+          (if (zero? (v))
+            zero
+            nonzero))
+        (rack:cstate next e s k)]
+       [(rack:kf:let-values ids body)
+        (define e+
+          (for/fold ([e+ e])
+              ([i (in-list ids)]
+               [v (in-vector vs)])
+            (hash-set e+ i v)))
+        (rack:cstate body e+ s k)]
+       [(rack:kf:wcm:key mv body)
+        (rack:cstate mv e s (rack:k e k mt-ms (rack:kf:wcm:val (v) body)))]
+       [(rack:kf:wcm:val mk body)
+        (rack:cstate body e s (k-mark-set k mk (v)))]
+       [(rack:kf:app:fun args)
+        (rack:state:app (v) empty args e k s)]
+       [(rack:kf:app:args fun rvals (list))
+        (rack:state:app fun (cons (v) rvals) empty e k s)]
+       [(rack:kf:app:args fun rvals (cons arg args))
+        (rack:cstate
+         arg e s
+         (rack:k e k mt-ms
+                 (rack:kf:app:args fun (cons (v) rvals) args)))])]))
+
 ;; Evaluations
 
 (struct rack:evalctxt
