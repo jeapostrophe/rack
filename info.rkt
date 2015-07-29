48ac611506adc82a04263d0050ee1ce42fa36d7a
Jay McCarthy <jay.mccarthy@gmail.com>
2015-07-29 10:07:16 -0600

    deps

--- info.rkt
+++ info.rkt
@@ -1,4 +1,5 @@
 #lang info
 (define package "rack")
-(define deps '("base" "data-lib" "racket-llvm" "rackunit-lib" "typed-racket-lib"))
+(define deps '("unstable-lib"
+               "base" "data-lib" "racket-llvm" "rackunit-lib" "typed-racket-lib"))
 (define build-deps '())
